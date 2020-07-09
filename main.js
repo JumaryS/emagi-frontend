692ddbf17844dd6e8c0a039342a305181b924c8b
JumarySanchez <sanchezjumary@gmail.com>
2020-07-09 15:17:27 -0400

    just a few changes

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 // first youll need to bring the files 
 
 const search = require('./emagi-search.js');
-const words = require('./emagi-words.js');
 const symbols = require('./emagi-symbols.js');
 const random = require('./emagi-random.js');
 const emagify = require('./emagify.js')
@@ -14,17 +13,20 @@ let start= process.argv.slice(2)
 
 // const usersInput = function(){ the function is not needed 
     // youll need to find what comes after start(process.argv) so ill need an if statement
-    if(start.includes('convert')){
+    if(start[0].includes('convert')){
         console.log(emagify(start.slice(1)))
         }// ill need an else if statment when 
     else if (start.includes('search')){
         console.log(search(start.slice(1)))
-    }else if(start.includes('random')){
-        console.log(random(start.slice(1)))
     }
-    else if (start === ''){
-        console.log()
+    else if(start[0].includes('random')){
+        console.log(random(symbols))
     }
+    else{
+        console.log("please put a valid input")
+    }
+    
+    
     
     // return start
     
